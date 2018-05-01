a4b715f47b3d735e2d65c9ca58db4ffa20b21d23
wade welch <wade.c.welch@gmail.com>
2018-05-01 11:58:42 -0500

    tip calculator first try

---
+++ tipcalc.py
@@ -0,0 +1,7 @@
+def tipcalc(num1, num2):
+    return num1 * num2
+
+
+number1 = float(input('enter the total amount '))
+number2 = float(input('enter the tip percentage as a decimal '))
+print(tipcalc(number1,number2))
