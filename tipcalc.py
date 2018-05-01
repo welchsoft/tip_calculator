3cf1a603501075c5803f22f5196dbc2c18358023
wade welch <wade.c.welch@gmail.com>
2018-05-01 14:12:07 -0500

    added comments

--- tipcalc.py
+++ tipcalc.py
@@ -1,7 +1,9 @@
+#define function that multiplies total and tip percentage
 def tipcalc(num1, num2):
     return num1 * num2
 
-
+#take user input
 number1 = float(input('enter the total amount '))
 number2 = float(input('enter the tip percentage as a decimal '))
+#call the function and print result
 print(tipcalc(number1,number2))
